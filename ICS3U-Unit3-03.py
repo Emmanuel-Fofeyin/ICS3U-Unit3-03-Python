870ee63f69cceee47d04f4f2c1df19cdc23c64e6
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-10-18 04:00:19 +0000

    made changes

--- ICS3U-Unit3-03.py
+++ ICS3U-Unit3-03.py
@@ -2,21 +2,23 @@
 
 # Created by: Emmanuel
 # Created on: Oct 2022
-# This program is a number guessing game using an else statement
+# This program is a number guessing game 
+# Using if,then and else statements.
 
 import random
+
 
 def main():
 
     # input
-    random_number = random.randint(0,9)
+    random_number = random.randint(0, 9)
     guessed_number = int(input("Guess a number from 0 - 9: "))
 
     # process and output
     if guessed_number == random_number:
         print("\nYou guessed correctly.")
     else:
-         print("\nIncorrect, the number was {}.".format(random_number))
+        print("\nIncorrect, the number was {}.".format(random_number))
 
     print("\n\nDone.")
 
